7baf1b3a7edd8367a26aee69e4ba9c01cee7baf5
Kevin <kevin.r.neely@outlook.com>
2018-06-01 10:56:32 -0400

    First Commit

---
+++ Domain/Context/KevinDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Domain.Context
+{
+    public class KevinDbContext : DbContext  
+    {  
+        public DbSet<User> ObjRegisterUser { get; set; } // Here User is the class
+    }  
+}
---
+++ Domain/User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class User  
+    {  
+        [Key]  
+        public int RegistrationId  
+        {  
+            get;  
+            set;  
+        } //This will be primary key column with auto increment  
+        public string FirstName  
+        {  
+            get;  
+            set;  
+        }  
+        public string LastName  
+        {  
+            get;  
+            set;  
+        }  
+        public string UserName  
+        {  
+            get;  
+            set;  
+        }  
+        public string EmailId  
+        {  
+            get;  
+            set;  
+        }  
+        public string Password  
+        {  
+            get;  
+            set;  
+        }  
+        public string Gender  
+        {  
+            get;  
+            set;  
+        }  
+        public string VCode  
+        {  
+            get;  
+            set;  
+        }  
+        public DateTime CreateDate  
+        {  
+            get;  
+            set;  
+        }  
+        public DateTime ModifyDate  
+        {  
+            get;  
+            set;  
+        }  
+        public bool Status  
+        {  
+            get;  
+            set;  
+        }  
+    }  
+}
